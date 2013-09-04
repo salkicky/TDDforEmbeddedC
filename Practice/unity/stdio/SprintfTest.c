2800377020f7a81198ea9a4e9980287a7a92c1d9
salkicky <salkicky@gmail.com>
2013-09-04 12:40:09 +0900

    add %s format test

--- Practice/unity/stdio/SprintfTest.c
+++ Practice/unity/stdio/SprintfTest.c
@@ -2,8 +2,17 @@
 
 TEST(sprintf, NoFormatOperations)
 {
-    char output[5];
+    char output[5] + "";
 
     TEST_ASSERT_EQUAL(3, sprintf(output, "hey"));
     TEST_ASSERT_EQUAL_STRING("hey", output);
+}
+
+
+TEST(sprintf, InsertString)
+{
+    char output[20] = "";
+
+    TEST_ASSERT_EQUAL(12, sprintf(output, "Hello %s\n", "World"));
+    TEST_ASSERT_EQUAL_STRING("Hello World\n", output);
 }
