4ab2d136952df0e1547471276330d2bdb1fdbf04
salkicky <salkicky@gmail.com>
2013-09-11 12:46:32 +0900

    modify LedDriver_TurnOn for multiple LEDs, and Test Failed.

--- Practice/unity/LedDriver/LedDriver.c
+++ Practice/unity/LedDriver/LedDriver.c
@@ -12,6 +12,7 @@ void LedDriver_Create(UINT16 * address)
 void LedDriver_TurnOn(int led_number)
 {
     *led_address = 1;
+    *led_address |= (1 << led_number);
 }
 
 void LedDriver_TurnOff(int led_number)
