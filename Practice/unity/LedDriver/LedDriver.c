a7004c329da54ce4cd3f37007d2557e0c3e644d1
salkicky <salkicky@gmail.com>
2013-09-11 12:57:05 +0900

    refactoring LedDriver_TurnOn, and add a helper for convert LedNumber to bit

--- Practice/unity/LedDriver/LedDriver.c
+++ Practice/unity/LedDriver/LedDriver.c
@@ -1,20 +1,37 @@
 #include <stdlib.h>
 #include "LedDriver.h"
 
+
+//-- static variable -------------------------------
 static UINT16   *led_address;
 
+
+//-- prototype --------------------------------------
+static UINT16 convertLedNumberToBit(int led_numer);
+
+
+//-- function ---------------------------------------
+// Create, and initialize LEDs
 void LedDriver_Create(UINT16 * address)
 {
     led_address = address;
     *led_address = 0;
 }
 
+// TurnOn LED
 void LedDriver_TurnOn(int led_number)
 {
-    *led_address |= (1 << (led_number - 1));
+    *led_address |= convertLedNumberToBit(led_number); 
 }
 
+// Turn off LED
 void LedDriver_TurnOff(int led_number)
 {
     *led_address = 0;
+}
+
+// convert LED number to bit
+UINT16 convertLedNumberToBit(int led_number)
+{
+    return (1 << (led_number - 1));
 }
