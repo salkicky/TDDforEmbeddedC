644183e63c691bd4ee4dece9325dee02fa56709f
salkicky <salkicky@gmail.com>
2013-09-17 17:48:35 +0900

    add RuntimeErrorStub

--- Practice/unity/mocks/RuntimeErrorStub.c
+++ Practice/unity/mocks/RuntimeErrorStub.c
@@ -5,17 +5,27 @@ static int parameter = -1;
 static const char * file = 0;
 static int line = -1;
 
-
 void RuntimeErrorStub_reset(void)
 {
     message = "No Error";
     parameter = 1;
 }
 
-const char * RuntimeErrorStub_getLastParameter(void)
+const char * RuntimeErrorStub_getLastError(void)
 {
     return message;
 }
 
 void RunTimeError(const char *m, int p, const char * f, int l)
+{
+    message = m;
+    parameter = p;
+    file = f;
+    line = 1;
+}
+
+int RuntimeErrorStub_getLastParameter(void)
+{
+    return parameter;
+}
     
