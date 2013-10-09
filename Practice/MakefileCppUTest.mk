f593b078629c8f0359d04675393a36bd2e735d2b
salkicky <salkicky@gmail.com>
2013-10-09 22:45:23 +0900

    add Makefile for CppUTest, and rename Makefile for Unity

---
+++ Practice/MakefileCppUTest.mk
@@ -0,0 +1,40 @@
+#This makefile makes all the main book code with CppUTest test harness
+
+#Set this to @ to keep the makefile quiet
+SILENCE = @
+
+#---- for environment ----------
+export CC="gcc"
+
+#---- Outputs ----#
+COMPONENT_NAME = BookCode_CppUTest
+
+#--- Inputs ----#
+CPPUTEST_HOME = CppUTest
+CPP_PLATFORM = gcc
+PROJECT_HOME_DIR = .
+
+SRC_DIRS = \
+	. \
+	$(PROJECT_HOME_DIR)/src/CpputestSample \
+
+TEST_SRC_DIRS = \
+	.\
+	$(PROJECT_HOME_DIR)/src/CpputestSample \
+	
+INCLUDE_DIRS =\
+  .\
+  $(PROJECT_HOME_DIR)/src/CpputestSample \
+  $(CPPUTEST_HOME)/include\
+
+MOCKS_SRC_DIRS = \
+	mocks\
+	
+CPPUTEST_WARNINGFLAGS = -Wall -Wswitch-default -Werror 
+#CPPUTEST_CFLAGS = -std=c89 
+CPPUTEST_CFLAGS += -Wall -Wstrict-prototypes -pedantic
+#LD_LIBRARIES = -lpthread
+	
+  
+include $(CPPUTEST_HOME)/build/MakefileWorker.mk
+
