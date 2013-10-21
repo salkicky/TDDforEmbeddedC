8b873779706ed4b09a5ba108df907c26eca95070
salkicky <salkicky@gmail.com>
2013-10-21 12:35:08 +0900

    add Fail TEST 'ScheduleOnEverydayItsTime'

--- Practice/tests/HomeAutomation/LightSchedulerTest.cpp
+++ Practice/tests/HomeAutomation/LightSchedulerTest.cpp
@@ -29,18 +29,6 @@ TEST_GROUP(LightScheduler)
 //  FAIL("Start here");
 //}
 
-// [TEST] 毎日ＰＭ８時にライトをＯＮする
-TEST(LightScheduler, ScheduleOneEverydayNotTimeYet)
-{
-	LightScheduler_scheduleTurnOn(1, EVERYDAY, 60 * 20);
-	FakeTimeService_setDay(MONDAY);
-	FakeTimeService_setMinuite(60 * 20 -1);
-	LightScheduler_wakeup();
-
-	LONGS_EQUAL(LIGHT_ID_UNKNOWN, LightControllerSpy_getLastId());
-	LONGS_EQUAL(LIGHT_STATE_UNKNOWN, LightControllerSpy_getState());
-}
-
 // [TEST] 初期化時の内部状態を確認する
 TEST(LightScheduler, NoChangeToLightsDuringInitialization)
 {
@@ -56,4 +44,28 @@ TEST(LightScheduler, NoScheduleNothingHappens)
 	LightScheduler_wakeup();
 	CHECK_EQUAL(LIGHT_STATE_UNKNOWN, LightControllerSpy_getState());
 	CHECK_EQUAL(LIGHT_STATE_UNKNOWN, LightControllerSpy_getLastId());
+}
+
+// [TEST] 毎日ＰＭ８時にライトをONする設定をして、まだ時間が来ない
+TEST(LightScheduler, ScheduleOnEverydayNotTimeYet)
+{
+	LightScheduler_scheduleTurnOn(1, EVERYDAY, 60 * 20);
+	FakeTimeService_setDay(MONDAY);
+	FakeTimeService_setMinuite(60 * 20 -1);
+	LightScheduler_wakeup();
+
+	LONGS_EQUAL(LIGHT_ID_UNKNOWN, LightControllerSpy_getLastId());
+	LONGS_EQUAL(LIGHT_STATE_UNKNOWN, LightControllerSpy_getState());
+}
+
+// [TEST] turn on every day.
+TEST(LightScheduler, ScheduleOnEverydayItsTimke)
+{
+    LightScheduler_scheduleTurnOn(1, EVERYDAY, 60*20);
+    FakeTimeService_setDay(MONDAY);
+    FakeTimeService_setMinuite(60*20);
+    LightScheduler_wakeup();
+
+    LONGS_EQUAL(1, LightControllerSpy_getLastId());
+	LONGS_EQUAL(LIGHT_ON, LightControllerSpy_getState());
 }
