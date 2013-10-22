cccad46239962792a1da7bc70585fe26bfda6d43
salkicky <salkicky@gmail.com>
2013-10-23 02:25:48 +0900

    add executeScheduledEvent()

--- Practice/src/HomeAutomation/LightScheduler.c
+++ Practice/src/HomeAutomation/LightScheduler.c
@@ -23,11 +23,30 @@ static SCHEDULED_LIGHT_EVENT _scheduled_event;
 /**************************************************
  * イベントを登録する
  **************************************************/
-static void scheduleEvent(int id, DAY day, long minuite, EVENT event)
+static void scheduleEvent(int id, DAY day, long minuite_of_day, EVENT event)
 {
 	_scheduled_event.id = id;
     _scheduled_event.event = event;
-    _scheduled_event.minuite_of_day = minuite;
+    _scheduled_event.minuite_of_day = minuite_of_day;
+}
+
+/**************************************************
+ * イベントを実行する
+ **************************************************/
+static void executeScheduledEvent(Time *time, SCHEDULED_LIGHT_EVENT *scheduled_event)
+{
+    if (scheduled_event->id == UN_USED) {
+        return;
+    }
+    if (scheduled_event->minuite_of_day != time->minuite_of_day) {
+        return;
+    }
+
+	if (scheduled_event->event == TURN_OFF) {
+    	LightController_TurnOff(scheduled_event->id);
+	} else if (scheduled_event->event == TURN_ON) {
+		LightController_TurnOn(scheduled_event->id);
+	}
 }
 
 /* ---------------------------------------- */
@@ -55,33 +74,22 @@ void LightScheduler_wakeup(void)
 
     TimeService_getTime(&time);
 
-    if (_scheduled_event.id == UN_USED) {
-        return;
-    }
-    if (_scheduled_event.minuite_of_day != time.minuite_of_day) {
-        return;
-    }
-
-	if (_scheduled_event.event == TURN_OFF) {
-    	LightController_TurnOff(_scheduled_event.id);
-	} else if (_scheduled_event.event == TURN_ON) {
-		LightController_TurnOn(_scheduled_event.id);
-	}
+	executeScheduledEvent(&time, &_scheduled_event);
 }
 
 /**************************************************
  * ONイベントのスケジュールを登録する
  **************************************************/
-void LightScheduler_scheduleTurnOn(int id, DAY day, long minuite)
+void LightScheduler_scheduleTurnOn(int id, DAY day, long minuite_of_day)
 {
-	scheduleEvent(id, day, minuite, TURN_ON);
+	scheduleEvent(id, day, minuite_of_day, TURN_ON);
 }
 
 /**************************************************
  * OFFイベントのスケジュールを登録する
  **************************************************/
-void LightScheduler_scheduleTurnOff(int id, DAY day, long minuite)
+void LightScheduler_scheduleTurnOff(int id, DAY day, long minuite_of_day)
 {
-	scheduleEvent(id, day, minuite, TURN_OFF);
+	scheduleEvent(id, day, minuite_of_day, TURN_OFF);
 }
 
